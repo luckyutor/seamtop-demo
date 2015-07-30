0363a27b197fcd987fc320fcc766a0c8756bdd6a
luckyutor <better_feng@126.com>
2015-07-30 20:04:52 +0800

    整理。

---
+++ src/main/java/com/seamtop/demo/hadoop/sort/Sort.java
@@ -0,0 +1,15 @@
+package com.seamtop.demo.hadoop.sort;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * Created by feng on 2015/7/30.
+ */
+public class Sort {
+    public static class Map extends Mapper<Object,Text,IntWritable,IntWritable>{
+
+        public
+    }
+}
