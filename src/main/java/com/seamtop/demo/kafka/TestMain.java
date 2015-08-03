38f3fc5ce87d1c05aebbe19e6af8e091cfcdbdc1
zongjunfeng <zongjunfeng@sohu-inc.com>
2015-08-03 17:27:43 +0800

    增加连表功能程序

---
+++ src/main/java/com/seamtop/demo/kafka/TestMain.java
@@ -0,0 +1,14 @@
+package com.seamtop.demo.kafka;
+/**
+ */
+public class TestMain
+{
+    public static void main(String[] args)
+    {
+        KafkaProducer producerThread = new KafkaProducer(KafkaProperties.topic);
+        producerThread.start();
+
+        KafkaConsumer consumerThread = new KafkaConsumer(KafkaProperties.topic);
+        consumerThread.start();
+    }
+}
