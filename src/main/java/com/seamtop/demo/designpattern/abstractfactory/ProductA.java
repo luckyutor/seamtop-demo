8fa90bd1bc6a4a11d1c78e569c3fc9257c50b801
luckyutor <better_feng@126.com>
2015-08-07 23:44:30 +0800

    增加设计模式-工厂模式

---
+++ src/main/java/com/seamtop/demo/designpattern/abstractfactory/ProductA.java
@@ -0,0 +1,11 @@
+package com.seamtop.demo.designpattern.abstractfactory;
+
+/**
+ * Created by feng on 2015/8/7.
+ */
+public class ProductA implements IProduct {
+
+    public void say(){
+        System.out.println("Product -- A");
+    }
+}
